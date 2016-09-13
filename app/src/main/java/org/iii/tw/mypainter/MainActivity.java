522b4b460dfc7a6559b78370f21ab7bb0625ed53
chuan1983 <chuan1983126@gmail.com>
2016-09-13 16:42:37 +0800

    v5

--- app/src/main/java/org/iii/tw/mypainter/MainActivity.java
+++ app/src/main/java/org/iii/tw/mypainter/MainActivity.java
@@ -2,12 +2,23 @@ package org.iii.tw.mypainter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyView myView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myView = (MyView)findViewById(R.id.myview);
+        myView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("brad","setonClick")
+            }
+        });
     }
 }
--- app/src/main/java/org/iii/tw/mypainter/MyView.java
+++ app/src/main/java/org/iii/tw/mypainter/MyView.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -15,7 +16,7 @@ import android.view.View;
 public class MyView extends View {
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
-    setOnClickListener(new MyClick());
+//    setOnClickListener(new MyClick());
     }
 
     @Override
@@ -26,10 +27,25 @@ public class MyView extends View {
         p.setStrokeWidth(4);
         canvas.drawLine(0,0,100,100,p);
     }
-    private class MyClick implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-            Log.d("brad","onclick");
-        }
+//    private class MyClick implements View.OnClickListener{
+//        @Override
+//        public void onClick(View v) {
+//            Log.d("brad","onclick");
+//        }
+//    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //return super.onTouchEvent(event);
+        float ex = event.getX();
+        float ey = event.getY();
+        Log.d("brad", "onTouchEvent" + ex + "x" + ey);
+        invalidate();
+        return true;
     }
+
+//    @Override
+//    public void setOnClickListener(OnClickListener l) {
+//        super.setOnClickListener(l);
+//    }
 }
